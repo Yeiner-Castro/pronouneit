590c430d9e7fad87dc3024036bc41eed828caa3a
Yeiner-Castro <yeiner.castro@unillanos.edu.co>
2024-05-08 15:54:01 -0500

    update-EjercicioRealizadoModel

--- models/EjercicioRealizadoModel.go
+++ models/EjercicioRealizadoModel.go
@@ -9,6 +9,7 @@ type EjercicioRealizado struct {
 	Resultado   int       `gorm:"not null"`
 	UsuarioID   uint      `gorm:"not null"`
 	EjercicioID uint      `gorm:"not null"`
+	Aprobado    bool      `gorm:"default:false"` // Añade esto
 	Usuario     Usuario   `gorm:"foreignKey:UsuarioID"`
 	Ejercicio   Ejercicio `gorm:"foreignKey:EjercicioID"`
 }
